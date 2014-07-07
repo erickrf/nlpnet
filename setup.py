9f4581d7c4a082d9bcaf21691b722625ffe06800
Erick Fonseca <erickrfonseca@gmail.com>
2014-07-07 16:59:13 -0300

    setup with version number 1.1.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
                  'bin/nlpnet-train.py',
                  'bin/nlpnet-test.py'],
       license = 'MIT',
-      version = '1.1.0',
+      version = '1.1.2',
       author = 'Erick Fonseca',
       author_email = 'erickrfonseca@gmail.com',
       url = 'http://nilc.icmc.usp.br/nlpnet',
