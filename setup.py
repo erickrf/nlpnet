b5b453219cd80736ff4ec55cca29bdb71f19f4ed
Erick Fonseca <erickrfonseca@gmail.com>
2017-05-04 18:34:40 -0300

    minor improvements in setup, solved problems from merge

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-import sys
-import warnings
+import numpy as np
 from distutils.core import setup
 from distutils.extension import Extension
+
 
 def readme():
     with open('README.rst') as f:
@@ -9,30 +9,30 @@ def readme():
     return text
 
 setup(
-    name = 'nlpnet',
-    description = 'Neural networks for NLP tasks',
-    packages = ['nlpnet', 'nlpnet.pos', 'nlpnet.srl', 'nlpnet.parse'],
-    ext_modules = [
+    name='nlpnet',
+    description='Neural networks for NLP tasks',
+    packages=['nlpnet', 'nlpnet.pos', 'nlpnet.srl', 'nlpnet.parse'],
+    ext_modules=[
         Extension(
             "nlpnet.network", 
             ["nlpnet/network.c"],
-            include_dirs=['.']
+            include_dirs=['.', np.get_include()]
         )
     ],
-    scripts = [
+    scripts=[
         'bin/nlpnet-tag.py',
         'bin/nlpnet-train.py',
         'bin/nlpnet-test.py',
         'bin/nlpnet-load-embeddings.py'
     ],
-     install_requires=[
+    install_requires=[
         'numpy>=1.9.0',
         'nltk>=3.2.2'
     ],
-    license = 'MIT',
-    version = '1.2.1',
-    author = 'Erick Fonseca',
-    author_email = 'erickrfonseca@gmail.com',
-    url = 'http://nilc.icmc.usp.br/nlpnet',
-    long_description = readme()
+    license='MIT',
+    version='1.2.1',
+    author='Erick Fonseca',
+    author_email='erickrfonseca@gmail.com',
+    url='http://nilc.icmc.usp.br/nlpnet',
+    long_description=readme()
 )
